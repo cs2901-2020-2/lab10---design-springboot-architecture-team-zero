12eb08951185ee3f6e5d6d25709ed20de9f2ce25
root <root@DESKTOP-OO1V4NK.localdomain>
2020-10-21 10:13:33 -0500

    Usuario

--- src/main/java/lab10/Usuario.java
+++ src/main/java/lab10/Usuario.java
@@ -1,1 +1,129 @@
 package lab10;
+
+import java.util.Date;
+
+public class Usuario{
+    private String hash;
+    private Integer sexo;
+    private Long codigo;
+    private String primerNombre;
+    private Integer vigencia;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String email;
+    private String nombre;
+    private String paswd;
+    private String segundoNombre;
+    private Date fechaCase;
+    private String dni;
+    private String nombreCompleto;
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public Integer getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Integer sexo) {
+        this.sexo = sexo;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public Integer getVigencia() {
+        return vigencia;
+    }
+
+    public void setVigencia(Integer vigencia) {
+        this.vigencia = vigencia;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaswd() {
+        return paswd;
+    }
+
+    public void setPaswd(String paswd) {
+        this.paswd = paswd;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public Date getFechaCase() {
+        return fechaCase;
+    }
+
+    public void setFechaCase(Date fechaCase) {
+        this.fechaCase = fechaCase;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+}
