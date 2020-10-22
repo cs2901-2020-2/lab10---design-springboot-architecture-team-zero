98e99b165a5d5e6313e80b3750b7522143b2010d
vostolaza <victor.ostolaza@utec.edu.pe>
2020-10-22 09:01:28 -0500

    ResponseEntity fix, ServiceApplication

--- src/main/java/com/example/demo/AuthenticationController.java
+++ src/main/java/com/example/demo/AuthenticationController.java
@@ -6,16 +6,16 @@ public class AuthenticationController{
     private UsuarioService usuarioService;
 
     public ResponseEntity<Usuario> create(Usuario user){
-        return usuarioService.create(user);
+        return ResponseEntity.accepted().body(usuarioService.create(user));
     }
 
     public ResponseEntity<Usuario> read(Long id){
-        return usuarioService.findOne(id);
+        return ResponseEntity.accepted().body(usuarioService.findOne(id));
     }
 
     public ResponseEntity<Usuario> update(Long id, Usuario user){
         Usuario user_ = usuarioService.findOne(id);
         user_ = user;
-        return usuarioService.update(user_);
+        return ResponseEntity.accepted().body(usuarioService.update(user_));
     }
 }
--- src/main/java/com/example/demo/ServiceApplication.java
+++ src/main/java/com/example/demo/ServiceApplication.java
@@ -1,1 +1,16 @@
-package lab10;
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class ServiceApplication {
+    static final Logger logger = Logger.getLogger(ServiceApplication.class.getName());
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
